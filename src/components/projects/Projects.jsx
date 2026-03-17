6b09a064b55dcd5dbf83114ef77c6941deae4968
Yash Kewte <yashkewate33@gmail.com>
2026-03-17 09:47:01 +0530

    added changes in new branch

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ProjectCard from './ProjectCard'
 import ExpenseTrackerImg from '../../assets/ExpenseTracker.png'
 import PortfolioImg from '../../assets/portfolio.png'
